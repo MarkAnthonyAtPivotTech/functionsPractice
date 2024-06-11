8d6b4956fbf1173f608795f29122bf6746ca89eb
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2024-06-10 19:11:47 -0600

    Added points

--- functions1.js
+++ functions1.js
@@ -1,26 +1,43 @@
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+console.log(multiply(5,6));
+
+
+function addFourNums(n1,n2,n3,n4) {
+    let totalSum =  n1 + n2 + n3 + n4;
+    return totalSum;
+}
+console.log(addFourNums(10,20,30,500))
+
+// Even or Odd Function
+//     Write a function that takes a number as an argument and returns true if the number is even, otherwise false.
+
+function isNumEven(num) {
+
+    if (num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+console.log(isNumEven(3))
+console.log(isNumEven(300))
+
 /*
-    Multiplication Function
-        Write a function that takes two numbers as arguments and returns their product. 
-        
-        Ex: passing 10 and 5 returns 500
     
-    Addition Function
-        Write a function that takes in four numbers and returns the sum of those numbers.
 
-        Ex: passing 2, 4, 6, 8 should return 20.
-    
-    Even or Odd Function
-        Write a function that takes a number as an argument and returns true if the number is even, otherwise false.
-
-    Is A Prime
+    Is A Prime - 1
         Write a function thats a number and returns true or false depending on whether the number is Prime (prime numbers are only divisible by themselves and by 1 and are not cleanly divisible by 2, 3, 5, or 7)
     
-    Highest Number Function
+    Highest Number Function - 1
         Write a function that takes in three numbers and returns the number that is the highest. 
         
         Ex: passing 200, 200, and 199 returns 200.
     
-    Multiplying Digits Function
+    Multiplying Digits Function - 2
         Write a function that takes a number as an argument and returns the factorial of that number (e.g., factoral 5 = 5*4*3*2*1 = 120).
 
         Ex: passing in 5 should return 120;
--- functions2.js
+++ functions2.js
@@ -1,27 +1,39 @@
-/*
-    String Reversal Function
-        Write a function that takes a string as an argument and returns the reversed string. 
+// String Reversal Function
+    // Write a function that takes a string as an argument and returns the reversed string. 
         
-        Ex: passing in 'Rick and Morty' should return 'ytroM dna kciR'
+    // Ex: passing in 'Rick and Morty' should return 'ytroM dna kciR'
 
-    Identify Season
+function stringReverse(string) {
+    let newString = string.split('');
+    newString = newString.reverse();
+    newString = newString.join('')
+
+
+    return newString;
+}
+
+console.log(stringReverse('Mark Anthony'))
+
+/*
+
+    Identify Season - 2
         Write a function that takes in the name of a month and returns what season that month is in.
 
         Ex: passing in 'January' should return the string 'Winter'
     
-    Convert To °F or °C
+    Convert To °F or °C - 2 
         Write a function that takes a number and a unit and then converts that number into Celcius or to Fahrenheit depending on the unit
 
         Ex: passing in 100 and Fahrenheit should return 212
         Ex: passing in 100 and Celcius should return 37.78
         Ex: passing only in 100 should return a string saying: 'Sorry, no unit was specified to convert to'.
 
-    Palindrome Function
+    Palindrome Function - 3
         Write a function that takes a string as an argument and returns true if the string is a palindrome (reads the same forwards and backwards), otherwise false.
 
         Ex: passing in 'Hello' should return false, passing in 'racecar' should return true.
     
-    Welcome to My Bar Function
+    Welcome to My Bar Function - 3
         Write a function that takes in two arguments - someone's name, someone's age, and the number of drinks they have had today - and returns a message saying whether or not that person is welcome into the bar. 
         
         Ex: passing in 'Max', 18, and 18 should return something like: 'Sorry, Max. You are not welcome into this bar because you are underage.`
